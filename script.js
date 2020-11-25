c4e182bb907c43e8b58af38a2588a537185895a0
nikhilaasnikar <nikhilasnikar@yahoo.com>
2020-11-25 15:55:12 +0000

    Implementing Play again Button

--- script.js
+++ script.js
@@ -16,10 +16,9 @@ console.log(document.querySelector('.message').textContent);
 
 // Event Handling Click Event
 
-const secretnumber = Math.trunc(Math.random() * 20) + 1;
+let secretnumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-console.log((document.querySelector('.number').textContent = secretnumber));
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -34,6 +33,7 @@ document.querySelector('.check').addEventListener('click', function () {
   //  When a Player Wins
   else if (guess === secretnumber) {
     document.querySelector('.message').textContent = '🎇🎇🎇 Correct Number!';
+    document.querySelector('.number').textContent = secretnumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -63,4 +63,24 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Start Guessing...';
+
+  //   How to hide again the value of secret number
+  secretnumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+
+  //   How to restore the score value
+
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  //How to return the value of input feild to blank
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
