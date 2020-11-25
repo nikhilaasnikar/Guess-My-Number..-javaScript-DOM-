d7a2e2764c077a2717d9a5404bb1f398abbda964
nikhilaasnikar <nikhilasnikar@yahoo.com>
2020-11-25 16:02:33 +0000

    adding comments

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ console.log(document.querySelector('.message').textContent);
 let secretnumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
