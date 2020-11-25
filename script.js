5cb5814c0f620bbba8795f38b281c862d63a0871
nikhilaasnikar <nikhilasnikar@yahoo.com>
2020-11-25 12:36:26 +0000

    handling Click Events

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Grabbing html element
+
+console.log(document.querySelector('.message').textContent);
+
+// Changing the text Content of Html element
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+// Event Handling Click Event
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '💀 No Number Entered!';
+  }
+});
